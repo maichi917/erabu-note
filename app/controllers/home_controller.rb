e00f1ad15532495c67a15e556e573090cb20571c
maichi917 <2525mk.917@gmail.com>
2026-08-17 21:42:20 +0900

    ホームに「今使っているもの」セクションを追加する (#287)
    - 使用中アイテムをカード表示し、なくなりそうフラグのトグルと
      感想を書く導線を置く
    - 他の一覧ページと見た目を揃える(見出し余白・使用中バッジ)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
-  def show; end
+  def show
+    @in_use_usage_logs = current_user.usage_logs
+                                      .in_use
+                                      .includes(:item)
+                                      .order(started_at: :desc)
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -15,4 +15,42 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to new_user_session_path
   end
+
+  test "home shows in-use items with low stock toggle and review link" do
+    user = users(:one)
+    sign_in user
+
+    item = items(:one)
+    item.start_using!(user, Time.zone.local(2026, 5, 10))
+    usage_log = item.current_usage_log
+
+    get home_path
+
+    assert_response :success
+    assert_select "h2", text: "今使っているもの"
+    assert_select "a[href='#{item_path(item)}']", text: item.name
+    assert_select "form[action='#{toggle_low_stock_item_path(item)}']"
+    assert_select "a[href='#{edit_usage_log_path(usage_log)}']", text: "感想を書く"
+  end
+
+  test "home shows empty state when nothing is in use" do
+    sign_in users(:one)
+
+    get home_path
+
+    assert_response :success
+    assert_select ".ui-empty-state", text: /使用中のアイテムがありません/
+  end
+
+  test "home does not show other user's in-use items" do
+    sign_in users(:one)
+
+    other_item = users(:two).items.create!(name: "他ユーザーのアイテム", in_stock: true)
+    other_item.start_using!(users(:two), Time.zone.local(2026, 5, 10))
+
+    get home_path
+
+    assert_response :success
+    assert_no_match "他ユーザーのアイテム", response.body
+  end
 end
